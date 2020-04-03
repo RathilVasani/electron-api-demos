03811dcdb59241493db667e330a32c1c424ac5af
rathil <rathilvasani@gmail.com>
2020-04-03 15:58:48 +0530

    remove redundant functionality in local

--- renderer-process/native-ui/local/local.js
+++ renderer-process/native-ui/local/local.js
@@ -1,9 +1,22 @@
 console.log("Hello local renderer");
 const {shell} = require('electron')
+const { exec } = require("child_process");
 const browserstack = require('browserstack-local')
 const bs_local = new browserstack.Local();
 const key = document.getElementById('accesskey').value
 const startlocal = document.getElementById('start-local')
+
+exec("./assets/BrowserStackLocal", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
 
 startlocal.addEventListener('click', (event) => {
 
@@ -11,20 +24,11 @@ startlocal.addEventListener('click', (event) => {
     console.log(key);
     console.log(bs_local.isRunning());
     if (!bs_local.isRunning()) {
-      // creates an instance of Local
-      // replace <browserstack-accesskey> with your key. You can also set an environment variable - "BROWSERSTACK_ACCESS_KEY".
        var args = '{"key": "'+key+'"}';
-
-
        bs_local_args = JSON.parse(args);
-       // bs_local_args = { key: key , args };
-       // bs_local_args = { key: key , forcelocal : true };
        console.log(bs_local_args);
-       // console.log(typeof bs_local_args);
-      // starts the Local instance with the required arguments
-      console.log(bs_local.isRunning());
+       console.log(bs_local.isRunning());
        bs_local.start(bs_local_args, function() {
-
          if (bs_local.isRunning()) {
            console.log("Started BrowserStackLocal");
            document.getElementById('local_status').innerHTML = "LocalTesting: Running";
@@ -32,25 +36,14 @@ startlocal.addEventListener('click', (event) => {
          else {
            console.log("Issue starting Binary from Code!!");
          }
-
          console.log(bs_local.isRunning());
-
-
 
       });
     }
     else{
       console.log('Binary Already Running!!');
     }
-
-
-
-
 });
-
-// function startlocal() {
-//
-// };
 
 
 const stoplocal = document.getElementById('stop-local')
